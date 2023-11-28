a46c1b06b15a4da552fe3ba7ad94f508d3626fac
Nguyễn Minh Thông - Paul Nguyen <thong.nm.28@gmail.com>
2023-11-28 17:00:41 +0700

    update

---
+++ 18.Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,11 @@
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(*) = (
+    SELECT MAX(order_count)
+    FROM (
+        SELECT COUNT(*) AS order_count
+        FROM Orders
+        GROUP BY customer_number
+    ) AS counts
+);
